a98a06b71dce7a0736f44b2ac678b051d81c34bf
mikekenndy <Mike.Kennedy.Dev@gmail.com>
2020-03-06 13:20:30 -0500

    some styling and js started on main page

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,19 @@
 // JS for homepage
+window.onscroll = function() {lockSidebar()};
+
+// Get the sidebar
+var sidebar = document.getElementById("sidebar");
+console.log(sidebar);
+
+// Get the offset position of the navbar
+var sticky_var = sidebar.offsetTop;
+
+// Add the sticky class to the sidebar when you reach its scroll position.  Remove "sticky" when you leave the scroll positon
+function lockSidebar() {
+	console.log(window.pageYOffset);
+	console.log(sticky_var);
+	if (window.pageYOffset > sticky_var)
+		sidebar.classList.add("sticky");
+	else
+		sidebar.classList.remove("sticky");
+}
