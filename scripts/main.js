628e34580ed4e0339a2ee27dfbcf8b5102111822
mikekenndy <Mike.Kennedy.Dev@gmail.com>
2020-03-06 16:36:23 -0500

    Added education section and sticky side-bar

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@
 window.onscroll = function() {lockSidebar()};
 
 // Get the sidebar
-var sidebar = document.getElementById("sidebar");
+var sidebar = document.getElementById("mySidebar");
 console.log(sidebar);
 
 // Get the offset position of the navbar
@@ -10,8 +10,6 @@ var sticky_var = sidebar.offsetTop;
 
 // Add the sticky class to the sidebar when you reach its scroll position.  Remove "sticky" when you leave the scroll positon
 function lockSidebar() {
-	console.log(window.pageYOffset);
-	console.log(sticky_var);
 	if (window.pageYOffset > sticky_var)
 		sidebar.classList.add("sticky");
 	else
